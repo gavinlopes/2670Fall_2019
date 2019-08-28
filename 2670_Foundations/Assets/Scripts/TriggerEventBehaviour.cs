9e76ab49ab6db10a078fde545881800598283375
Gavin <gavin.l.lopes>
2019-08-28 13:50:04 -0600

    On Trigger Enter/Stay

---
+++ 2670_Foundations/Assets/Scripts/TriggerEventBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventBehaviour : MonoBehaviour
+{
+
+   public UnityEvent TriggerEnterEvent, TriggerStayEvent;
+   
+   private void OnTriggerEnter(Collider other)
+   {
+      TriggerEnterEvent.Invoke();
+   }
+
+   private void OnTriggerStay(Collider other)
+   {
+     TriggerStayEvent.Invoke();
+   }
+}
