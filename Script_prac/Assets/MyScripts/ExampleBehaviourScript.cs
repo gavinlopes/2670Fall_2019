594b799a818a2d81ab289e9ae396a476f03fddfc
Gavin <gavin.l.lopes>
2019-09-04 11:27:48 -0600

    RGB_script_tutorial

---
+++ Script_prac/Assets/MyScripts/ExampleBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleBehaviourScript : MonoBehaviour
+{
+     void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+}
