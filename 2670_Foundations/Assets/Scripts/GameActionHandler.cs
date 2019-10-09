51889542424b62101e5694ce6b68578bf2467443
Gavin <gavin.l.lopes>
2019-10-09 14:17:04 -0600

    sick day
    do not remember a damn thing

---
+++ 2670_Foundations/Assets/Scripts/GameActionHandler.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameActionHandler : MonoBehaviour
+{
+    public GameAction gameActionObj;
+    public UnityEvent handlerEvent;
+
+    private void Start()
+    {
+        
+        
+    }
+}
