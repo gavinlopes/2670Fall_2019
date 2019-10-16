5ad4508767b033f8bda24a7701c347f7b05b182b
Gavin <gavin.l.lopes>
2019-10-16 15:21:13 -0600

    final_sub_10-16

---
+++ 2670_Foundations/Assets/Scripts/Vector3Data.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3Data : MonoBehaviour
+{
+    public Vector3 value;
+
+    public void UpdateValue(Transform transformObj)
+    {
+        value = transformObj.localPosition;
+    }
+}
