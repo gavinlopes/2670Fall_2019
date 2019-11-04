eb16ac1ca33f65e9b23f7b0f313ef5c3e96e843c
Gavin <gavin.l.lopes>
2019-11-04 14:23:40 -0700

    standard assets/game action

---
+++ 2790_scripting/Assets/Scripts/MartyClass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MartyClass : MonoBehaviour
+{
+
+
+    public void FMinus()
+    {
+        Debug.Log( message:"Rodayne");
+    }
+ 
+    void Start()
+    {
+        
+        
+
+    }
+
+}
---
+++ 2790_scripting/Assets/Scripts/RodayneClass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RodayneClass : MonoBehaviour
+{
+
+
+    public void Talk()
+    {
+        Debug.Log( message:"Rodayne");
+    }
+ 
+    void Start()
+    {
+        
+
+
+    }
+
+}
