7707845ed1013ddfea5207e7e508e98b2e2f70cd
Gavin <gavin.l.lopes>
2019-11-06 13:28:36 -0700

    mountain_range_update

---
+++ 2790_scripting/Assets/Scripts/RenderMatte.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderMatte : MonoBehaviour
+{
+    public BoolData useMatte;
+    public Material materialObj;
+    
+    void Start()
+    {
+        if (useMatte)
+        {
+           GetComponent<Renderer>().material = materialObj; 
+        }
+    }
+}
