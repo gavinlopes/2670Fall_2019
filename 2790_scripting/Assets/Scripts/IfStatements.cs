d15837ebaffacb98c45382b21a94f174da9f3c02
Gavin <gavin.l.lopes>
2019-11-11 14:25:47 -0700

    scripting fun

---
+++ 2790_scripting/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    public int a = 4;
+    public int b = 5;
+    public int c = 10;
+    public string password = "Friend";
+    public bool lightsOn = false;
+
+    void Start()
+    {
+        if (a+b < c)
+        {
+            print(true);
+        }
+        else if (!lightsOn)
+        {
+            print(true);
+        }
+        else
+        {
+            print(false);
+        }
+        
+    }
+}
---
+++ 2790_scripting/Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Movement : MonoBehaviour
+{
+    public Vector3 position;
+    public CharacterController controller;
+
+    public float moveSpeed = 10f, gravity = 9.81f, jumpSpeed = 30f;
+    public int jumpCount;
+    public int jumpCountMax = 2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.x = moveSpeed * Input.GetAxis("Horizontal");
+        position.z = moveSpeed * Input.GetAxis("Vertical");
+        position.y -= gravity;
+
+        if (controller.isGrounded)
+        {
+            position.y = 0;
+            jumpCount = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+        {
+            position.y = jumpSpeed;
+            jumpCount++;
+        }
+
+        controller.Move(position * Time.deltaTime);
+    }
+}
