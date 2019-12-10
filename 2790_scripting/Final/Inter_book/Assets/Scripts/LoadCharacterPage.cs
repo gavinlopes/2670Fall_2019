bd0e1f7c08da0d57f6af53886d8e6efa0a9f2b1b
Unknown <gavin.l.lopes@gmail.com>
2019-12-10 01:43:01 -0700

    finals

---
+++ 2790_scripting/Final/Inter_book/Assets/Scripts/LoadCharacterPage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine. SceneManagement;
+
+public class LoadCharacterPage : MonoBehaviour
+{
+    public void LoadingPageOf(string characterName)
+    {
+        SceneManager.LoadScene(characterName);
+    }
+
+}
